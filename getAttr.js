02356192b2f282bb54ee6c8cb267f0c3be99b755
ChenRuChao <1343644702@qq.com>
2015-09-09 10:45:52 +0800

    数据采集器1.0

---
+++ getAttr.js
@@ -0,0 +1,133 @@
+<div class="zlist">
+        <h2>部首为“丨”的汉字(共 47 字)</h2>
+        <li><A HREF="/z/15/js/4E28.htm" TARGET=_blank>丨</A></li>
+                <h3>1画</h3>
+                        <li><A HREF="/z/15/js/4E29.htm" TARGET=_blank>丩</A></li>
+                        <li><A HREF="/z/84/js/20062.htm" TARGET=_blank><img src="/p/?l=kj&u=20062&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20061.htm" TARGET=_blank><img src="/p/?l=kj&u=20061&s=20"></A></li>
+                <h3>2画</h3>
+                        <li><A HREF="/z/15/js/4E2B.htm" TARGET=_blank>丫</A></li>
+                        <li><A HREF="/z/15/js/4E2C.htm" TARGET=_blank>丬</A></li>
+                        <li><A HREF="/z/e/js/3403.htm" TARGET=_blank><img src="/p/?l=kj&u=3403&s=20"></A></li>
+                        <li><A HREF="/z/e/js/3404.htm" TARGET=_blank><img src="/p/?l=kj&u=3404&s=20"></A></li>
+               <h3>3画</h3>
+                        <li><A HREF="/z/15/js/4E2E.htm" TARGET=_blank>丮</A></li>
+                        <li><A HREF="/z/15/js/4E2F.htm" TARGET=_blank>丯</A></li>
+                        <li><A HREF="/z/15/js/4E30.htm" TARGET=_blank>丰</A></li>
+                        <li><A HREF="/z/15/js/4E2D.htm" TARGET=_blank>中</A></li>
+                        <li><A HREF="/z/84/js/20063.htm" TARGET=_blank><img src="/p/?l=kj&u=20063&s=20"></A></li>
+                        <li><A HREF="/z/ae/js/2A708.htm" TARGET=_blank><img src="/p/?l=kj&u=2A708&s=20"></A></li>
+               <h3>4画</h3>
+                        <li><A HREF="/z/15/js/4E31.htm" TARGET=_blank>丱</A></li>
+                        <li><A HREF="/z/16/js/5361.htm" TARGET=_blank>卡</A></li>
+                        <li><A HREF="/z/ac/js/29C0B.htm" TARGET=_blank><img src="/p/?l=kj&u=29C0B&s=20"></A></li>
+                        <li><A HREF="/z/ac/js/29C0A.htm" TARGET=_blank><img src="/p/?l=kj&u=29C0A&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20065.htm" TARGET=_blank><img src="/p/?l=kj&u=20065&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20064.htm" TARGET=_blank><img src="/p/?l=kj&u=20064&s=20"></A></li>
+                <h3>5画</h3>
+                        <li><A HREF="/z/84/js/20066.htm" TARGET=_blank><img src="/p/?l=kj&u=20066&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20067.htm" TARGET=_blank><img src="/p/?l=kj&u=20067&s=20"></A></li>
+                <h3>6画</h3>
+                        <li><A HREF="/z/15/js/4E32.htm" TARGET=_blank>串</A></li>
+                        <li><A HREF="/z/84/js/20068.htm" TARGET=_blank><img src="/p/?l=kj&u=20068&s=20"></A></li>
+                <h3>7画</h3>
+                        <li><A HREF="/z/16/js/5365.htm" TARGET=_blank>卥</A></li>
+                        <li><A HREF="/z/15/js/4E33.htm" TARGET=_blank>丳</A></li>
+                        <li><A HREF="/z/84/js/2006B.htm" TARGET=_blank><img src="/p/?l=kj&u=2006B&s=20"></A></li>
+                        <li><A HREF="/z/84/js/2006C.htm" TARGET=_blank><img src="/p/?l=kj&u=2006C&s=20"></A></li>
+                        <li><A HREF="/z/84/js/2006A.htm" TARGET=_blank><img src="/p/?l=kj&u=2006A&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20069.htm" TARGET=_blank><img src="/p/?l=kj&u=20069&s=20"></A></li>
+                <h3>8画</h3>
+                        <li><A HREF="/z/15/js/4E34.htm" TARGET=_blank>临</A></li>
+                        <li><A HREF="/z/84/js/2006E.htm" TARGET=_blank><img src="/p/?l=kj&u=2006E&s=20"></A></li>
+                        <li><A HREF="/z/84/js/2006D.htm" TARGET=_blank><img src="/p/?l=kj&u=2006D&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20070.htm" TARGET=_blank><img src="/p/?l=kj&u=20070&s=20"></A></li>
+                        <li><A HREF="/z/84/js/2006F.htm" TARGET=_blank><img src="/p/?l=kj&u=2006F&s=20"></A></li>
+                        <li><A HREF="/z/ae/js/2A709.htm" TARGET=_blank><img src="/p/?l=kj&u=2A709&s=20"></A></li>
+               <h3>9画</h3>
+                        <li><A HREF="/z/84/js/20071.htm" TARGET=_blank><img src="/p/?l=kj&u=20071&s=20"></A></li>
+                <h3>10画</h3>
+                        <li><A HREF="/z/84/js/20073.htm" TARGET=_blank><img src="/p/?l=kj&u=20073&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20072.htm" TARGET=_blank><img src="/p/?l=kj&u=20072&s=20"></A></li>
+                <h3>11画</h3>
+                        <li><A HREF="/z/84/js/20075.htm" TARGET=_blank><img src="/p/?l=kj&u=20075&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20074.htm" TARGET=_blank><img src="/p/?l=kj&u=20074&s=20"></A></li>
+                <h3>13画</h3>
+                        <li><A HREF="/z/84/js/20077.htm" TARGET=_blank><img src="/p/?l=kj&u=20077&s=20"></A></li>
+                        <li><A HREF="/z/84/js/20076.htm" TARGET=_blank><img src="/p/?l=kj&u=20076&s=20"></A></li>
+                <h3>14画</h3>
+                        <li><A HREF="/z/84/js/20078.htm" TARGET=_blank><img src="/p/?l=kj&u=20078&s=20"></A></li>
+                <h3>16画</h3>
+                        <li><A HREF="/z/84/js/20079.htm" TARGET=_blank><img src="/p/?l=kj&u=20079&s=20"></A></li>
+                 <h3>18画</h3>
+                        <li><A HREF="/z/84/js/2007A.htm" TARGET=_blank><img src="/p/?l=kj&u=2007A&s=20"></A></li>
+                  <h3>21画</h3>
+                        <li><A HREF="/z/84/js/2007B.htm" TARGET=_blank><img src="/p/?l=kj&u=2007B&s=20"></A></li>
+</div>
+<div class=hb></div>
+<hr>
+
+
+
+
+<td class="z_i_t2_py" align="center">
+        <span class="dicpy"><a href="/z/pyjs/?py=hao3" target="_blank">hǎo</a><script>spz("hao3");</script></span>
+        <span class="dicpy"><a href="/z/pyjs/?py=hao4" target="_blank">hào</a><script>spz("hao4");</script></span>
+</td>
+<td class="z_i_t2_py" align="center">
+        <span class="dicpy">
+            <a href="/z/zyjs/?zy=%E3%84%8F%E3%84%A0%CB%87" target="_blank">ㄏㄠˇ</a>
+            <script>spz("hao3");</script>
+        </span>
+        <span class="dicpy">
+                <a href="/z/zyjs/?zy=%E3%84%8F%E3%84%A0%CB%8B" target="_blank">ㄏㄠˋ</a>
+                <script>spz("hao4");</script>
+        </span>
+</td>
+<td class="z_i_t2">
+        <div class="z_it2_jbs">
+                 <a href="/z/jbs/?jbs=%E5%A5%B3" target="_blank">女</a>
+        </div>
+        <div class="z_it2_jbh">
+                 <a href="/z/jbs/?jbs=%E5%A5%B3&jbh=3" target="_blank">3</a>
+        </div>
+        <div class="z_it2_jzbh">
+                <a href="/z/jbs/zbh/?jzbh=6" target="_blank">6</a>
+        </div>
+</td>
+<td class="z_i_t2">
+        <center><a href="/z/zxjs/?zxjg=%E5%B7%A6%E5%8F%B3%E7%BB%93%E6%9E%84" target="_blank">左右结构</a></center>会意；从女、从子
+</td>
+        <td class="z_i_t2_ytz" >
+        <a href="/z/f/js/3740.htm" target="_blank"><img src="/p/?l=kj&u=3740&s=20"></a> 
+        <a href="/z/8a/js/21943.htm" target="_blank"><img src="/p/?l=kj&u=21943&s=20"></a>
+        <a href="/z/8a/js/21946.htm" target="_blank"><img src="/p/?l=kj&u=21946&s=20"></a> 
+        <a href="/z/8d/js/22452.htm" target="_blank"><img src="/p/?l=kj&u=22452&s=20"></a>
+        <a href="/z/97/js/24AE7.htm" target="_blank"><img src="/p/?l=kj&u=24AE7&s=20"></a>
+        <a href="/z/a9/js/29414.htm" target="_blank"><img src="/p/?l=kj&u=29414&s=20"></a> 
+</td>
+
+
+<tr>
+    <td class="z_i_t3_uno">
+            <span id="z_i_t3_uno_l">统一码UNICODE</span>
+    </td>
+    <td class="z_i_t3"><span id="z_i_t3_wb_l">五笔</span></td>
+    <td class="z_i_t3"><span id="z_i_t3_cj_l">仓颉</span></td>
+    <td class="z_i_t3">
+            <span id="z_i_t3_zm_l">郑码</span>
+    </td>
+    <td class="z_i_t3"><span id="z_i_t3_fc_l">四角</span></td>
+    <td class="z_i_t3_bis" align="left"><span id="z_i_t3_bis_l">笔顺</span></td>
+ </tr>
+<tr>
+        <td class="z_i_t4_uno" align="center"><a href="http://www.unicode.org/cgi-bin/GetUnihanData.pl?codepoint=597D" target=_blank> <img src="/images/unicode2.gif" border=0 align=absmiddle></a> U+597D</td>
+        <td class="z_i_t4" align="center"><span class="diczx7">VBG</span></td>
+        <td class="z_i_t4" align="center"><span class="diczx7">VND</span></td>
+        <td class="z_i_t4" align="center"><span class="diczx7">ZMYA</span></td>
+        <td class="z_i_t4" align="center"><span class="diczx6">47447</span></td>
+        <td class="z_i_t4" align="center"><span id="z_i_t2_bis" title="折撇横折竖横">フノ一フ丨一</span></td>
+</tr>
+
+
+
